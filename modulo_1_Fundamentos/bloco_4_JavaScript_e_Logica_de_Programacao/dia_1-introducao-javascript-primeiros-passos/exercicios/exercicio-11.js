67e3838bf15f14c8f99d446e2e2df04ae90b373f
Bruno <brunocfaraco@hotmail.com.br>
2022-05-13 01:33:20 -0300

    melhoramento do código do exercício 11

--- modulo_1_Fundamentos/bloco_4_JavaScript_e_Logica_de_Programacao/dia_1-introducao-javascript-primeiros-passos/exercicios/exercicio-11.js
+++ modulo_1_Fundamentos/bloco_4_JavaScript_e_Logica_de_Programacao/dia_1-introducao-javascript-primeiros-passos/exercicios/exercicio-11.js
@@ -1,44 +1,33 @@
-const salarioBruto = 3000;
+const salarioBruto = 30000;
 
 let aliquotaINSS = 0;
-let valorINSS = 0;
-let salarioDescontadoINSS = 0;
 let aliquotaIR = 0;
-let irAPagar = 0;
 
-if (salarioBruto <= 5189,82){
-    if (salarioBruto <= 1556.94) {
-        aliquotaINSS = 0.08;
-        valorINSS = salarioBruto * aliquotaINSS;
-    } else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
-        aliquotaINSS = 0.09;
-        valorINSS = salarioBruto * aliquotaINSS;
-    } else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82){
-        aliquotaINSS = 0.11;
-        valorINSS = salarioBruto * aliquotaINSS;
-    } else {
-        valorINSS = 570.88;
-    };
+if (salarioBruto <= 1556.94) {
+    aliquotaINSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+    aliquotaINSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11;
+} else {
+    aliquotaINSS = 570.88;
 };
 
-salarioDescontadoINSS = salarioBruto - valorINSS;
+const salarioBase = salarioBruto - aliquotaINSS;
 
-if (salarioDescontadoINSS <= 4664.68){
-    if (salarioDescontadoINSS <= 1903.98){
-        irAPagar = 0
-    } else if (salarioDescontadoINSS >= 1903.99 && salarioDescontadoINSS <= 2826.65) {
-        aliquotaIR = 0.075
-        irAPagar = (salarioDescontadoINSS * aliquotaIR) - 142.80;
-    } else if (salarioDescontadoINSS >= 2826.66 && salarioDescontadoINSS <= 3751.05) {
-        aliquotaIR = 0.15
-        irAPagar = (salarioDescontadoINSS * aliquotaIR) - 354.80;
-    } else if (salarioDescontadoINSS >= 3751.06 && salarioDescontadoINSS <= 4664.68) {
-        aliquotaIR = 0.225
-        irAPagar = (salarioDescontadoINSS * aliquotaIR) - 636.13;
-    };
-};
+if (salarioBase <= 1903.98){
+    aliquotaIR = 0
+} else if (salarioBase <= 2826.65) {
+    aliquotaIR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+    aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+    aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else {
+    aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
 
-let salarioLiquido = salarioDescontadoINSS - irAPagar;
+let salarioLiquido = salarioBase - aliquotaIR;
 
 console.log(salarioLiquido);
 
