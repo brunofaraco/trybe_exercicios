4e344f57ae151247a516416d4b8295cd28440ed0
Bruno <brunocfaraco@hotmail.com.br>
2022-05-13 19:07:18 -0300

    conclusão dos 5 primeiros exercícios

---
+++ modulo_1_Fundamentos/bloco_4_JavaScript_e_Logica_de_Programacao/dia_2-array-e-loop-for/exercicios/exercicios-1-ao-7.js
@@ -0,0 +1,63 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// exercício 1
+
+for (let index1 = 0; index1 < numbers.length; index1 += 1) {
+    console.log('Resposta do exercício 1:', numbers[index1]);
+};
+
+console.log('');
+
+// exercício 2
+
+let sumNumbersFirstWay = 0;
+
+for (let index2 = 0; index2 < numbers.length; index2 += 1) {
+    sumNumbersFirstWay = sumNumbersFirstWay + numbers[index2];
+};
+
+console.log('Resposta do exercício 2, 1º forma:', sumNumbersFirstWay);
+
+let sumNumbersSecondWay = 0;
+
+for (let number2 of numbers) {
+    sumNumbersSecondWay = sumNumbersSecondWay + number2;
+};
+
+console.log('Resposta do exercício 2, 2º forma:', sumNumbersSecondWay);
+console.log('');
+
+// exercício 3
+
+let sumNumbersThirdWay = 0;
+
+for (let number3 of numbers) {
+    sumNumbersThirdWay = sumNumbersThirdWay + number3;
+};
+
+let arithmeticAverage = sumNumbersThirdWay / numbers.length
+
+console.log('Resposta do exercício 3:', arithmeticAverage);
+console.log('');
+
+// exercício 4
+
+if (arithmeticAverage > 20) {
+    console.log('Resposta do exercício 4: Valor maior que 20.');
+} else {
+    console.log('Resposta do exercício 4: Valor menor ou igual a 20.');
+}
+console.log('');
+
+// exercício 5
+
+let highestNumber = 0;
+
+for (let index5 = 0; index5 < numbers.length; index5 += 1) {
+    if (highestNumber < numbers[index5]) {
+        highestNumber = numbers[index5];
+    }
+}
+
+console.log('Resposta do exercício 5:', highestNumber);
+console.log('');
