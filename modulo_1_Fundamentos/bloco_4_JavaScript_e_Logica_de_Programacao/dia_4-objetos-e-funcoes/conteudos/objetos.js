8cb262f0830817e8665ffdc01aa039cdfbb61249
Bruno <brunocfaraco@hotmail.com.br>
2022-05-17 16:20:48 -0300

    inicio dos exercicios 4.4

---
+++ modulo_1_Fundamentos/bloco_4_JavaScript_e_Logica_de_Programacao/dia_4-objetos-e-funcoes/conteudos/objetos.js
@@ -0,0 +1,13 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3 }
+};
+
+console.log(player['name']);
+console.log(player['lastName']);
+console.log(player['age']);
+
