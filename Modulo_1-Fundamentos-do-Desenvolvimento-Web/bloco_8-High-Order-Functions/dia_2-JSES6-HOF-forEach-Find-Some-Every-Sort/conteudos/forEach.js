182ac8c3f200517fb477af773d51ef39ca27e929
Bruno <brunocfaraco@hotmail.com.br>
2022-06-23 16:05:25 -0300

    criação da branch 8.2 e resolução do exercício pré aula de "forEach"

---
+++ Modulo_1-Fundamentos-do-Desenvolvimento-Web/bloco_8-High-Order-Functions/dia_2-JSES6-HOF-forEach-Find-Some-Every-Sort/conteudos/forEach.js
@@ -0,0 +1,18 @@
+// Para Fixar
+
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com'
+];
+
+const showEmailList = (email) => {
+    console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+
+// Adicione seu código aqui
+
+emailListInData.forEach(showEmailList);
