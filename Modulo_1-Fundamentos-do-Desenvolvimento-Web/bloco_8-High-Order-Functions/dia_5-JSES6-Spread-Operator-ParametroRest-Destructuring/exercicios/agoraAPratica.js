9b80feb965c0c3c0599107b09b00b05a2f79f1e1
Bruno <brunocfaraco@hotmail.com.br>
2022-06-29 13:24:24 -0300

    conclusão exercício nº6

--- Modulo_1-Fundamentos-do-Desenvolvimento-Web/bloco_8-High-Order-Functions/dia_5-JSES6-Spread-Operator-ParametroRest-Destructuring/exercicios/agoraAPratica.js
+++ Modulo_1-Fundamentos-do-Desenvolvimento-Web/bloco_8-High-Order-Functions/dia_5-JSES6-Spread-Operator-ParametroRest-Destructuring/exercicios/agoraAPratica.js
@@ -79,3 +79,41 @@
 // const swap = ([one, two, three]) => [three, two, one]
 
 // console.log(swap(myList));
+
+// 6 - Suponha que você esteja lidando com carros e, da forma como o problema lhe foi entregue, cada carro é modelado como um array. Porém, essa modelagem está baixo nível. Cria uma função toObject que, dada uma lista, retorna um objeto representando o carro:
+
+// const palio = ['Palio', 'Fiat', 2019];
+// const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+// const chiron = ['Chiron', 'Bugatti', 2016];
+
+// const toObject = ([name, brand, releaseYear]) => ( { name, brand, releaseYear, } )
+
+// console.log(toObject(palio));
+
+// 7 - Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
+
+// const ships = [
+//     {
+//         name: 'Titanic',
+//         length: 269.1,
+//         measurementUnit: 'meters',
+//     },
+//     {
+//         name: 'Queen Mary 2',
+//         length: 1132,
+//         measurementUnit: 'feet',
+//     },
+//     {
+//         name: 'Yamato',
+//         length: 256,
+//         measurementUnit: 'meters',
+//     },
+// ];
+
+// const shipLength = () => {
+
+// }
+
+// console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
