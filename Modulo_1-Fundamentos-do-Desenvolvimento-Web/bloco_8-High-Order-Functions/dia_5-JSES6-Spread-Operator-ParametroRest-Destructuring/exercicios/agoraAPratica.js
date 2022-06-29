af86ea151fe6f93a2fd38656278efe3c9d524143
Bruno <brunocfaraco@hotmail.com.br>
2022-06-29 13:33:17 -0300

    conclusão exercício nº8

--- Modulo_1-Fundamentos-do-Desenvolvimento-Web/bloco_8-High-Order-Functions/dia_5-JSES6-Spread-Operator-ParametroRest-Destructuring/exercicios/agoraAPratica.js
+++ Modulo_1-Fundamentos-do-Desenvolvimento-Web/bloco_8-High-Order-Functions/dia_5-JSES6-Spread-Operator-ParametroRest-Destructuring/exercicios/agoraAPratica.js
@@ -92,26 +92,34 @@
 
 // 7 - Escreva uma função shipLength que, dado um objeto representando um navio, retorna o comprimento dele, mostrando também a devida unidade de comprimento:
 
-const ships = [
-    {
-        name: 'Titanic',
-        length: 269.1,
-        measurementUnit: 'meters',
-    },
-    {
-        name: 'Queen Mary 2',
-        length: 1132,
-        measurementUnit: 'feet',
-    },
-    {
-        name: 'Yamato',
-        length: 256,
-        measurementUnit: 'meters',
-    },
-];
+// const ships = [
+//     {
+//         name: 'Titanic',
+//         length: 269.1,
+//         measurementUnit: 'meters',
+//     },
+//     {
+//         name: 'Queen Mary 2',
+//         length: 1132,
+//         measurementUnit: 'feet',
+//     },
+//     {
+//         name: 'Yamato',
+//         length: 256,
+//         measurementUnit: 'meters',
+//     },
+// ];
 
-const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`
+// const shipLength = ({name, length, measurementUnit}) => `${name} is ${length} ${measurementUnit} long`
 
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+// console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
+// console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
+// console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+
+// 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+
+const greet = (name = 'Anônimo', greet = 'Hi') => `${greet} ${name}`
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
